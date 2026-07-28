0fddf4a7fc2b8c330135ab370781c443de76627f
Karim Nafti <karim.nafti.10@gmail.com>
2026-07-28 19:26:13 +0100

    Add core_platform/templates to TEMPLATES DIRS to fix TemplateDoesNotExist for login.html

--- core_platform/settings.py
+++ core_platform/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'core_platform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'core_platform' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
