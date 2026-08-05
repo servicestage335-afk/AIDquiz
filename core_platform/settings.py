17fc511e7deb34cbe7de5ba2ca7dba2ede9c062d
Karim Nafti <karim.nafti.10@gmail.com>
2026-08-06 00:21:38 +0100

    Configure PostgreSQL database url and ssl require settings for core_platform and root settings

--- core_platform/settings.py
+++ core_platform/settings.py
@@ -94,8 +94,8 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(
         env='DATABASE_URL',
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
